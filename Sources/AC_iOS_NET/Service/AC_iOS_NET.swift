dbe3885b1c01867a98cf45874cb6a21b186b8f1d
Lammax <lammax@mail.ru>
2020-07-27 10:50:00 +0300

    Throw URLResponse upwards

--- Sources/AC_iOS_NET/Service/AC_iOS_NET.swift
+++ Sources/AC_iOS_NET/Service/AC_iOS_NET.swift
@@ -12,27 +12,27 @@ open class NET {
     open class Localizer {
         
         public typealias prepareCompletionHandler = (LocalizationModel.Prepare.Response?, URLResponse?, Error?) -> Void
-        public static func prepare(at serverAddress: String = Servers.addresses[2], for request: LocalizationModel.Prepare.Request, completion: @escaping prepareCompletionHandler) {
+        public static func prepare(at serverAddress: String = Servers.addresses[0], for request: LocalizationModel.Prepare.Request, completion: @escaping prepareCompletionHandler) {
             guard let url = REST.API.Localization.prepare.getUrl(for: serverAddress, additionalUrlPart: request.urlPart) else { completion(nil, nil, nil); return }
             //guard let data = try? JSONEncoder().encode(LocalizationModel.Prepare.Request(location: location)) else { completion(nil, nil, nil); return }
             
             nativeNet.dataTask(
                 with: REST.API.Localization.prepare.additionalHeaders,
                 with: url,
-                restMethod: REST.Method.GET) { sData, sResponse, sError in
-                    guard sError == nil else { completion(nil, nil, sError); return }
-                    guard let data = sData else { completion(nil, nil, nil); return }
+                restMethod: .GET) { sData, sResponse, sError in
+                    guard sError == nil else { completion(nil, sResponse, sError); return }
+                    guard let data = sData else { completion(nil, sResponse, nil); return }
                     
                     do {
                         //print(data)
                         //print(String(data: data, encoding: .utf8)!)
                         let prepareStatus = try JSONDecoder().decode(LocalizationModel.Prepare.Response.self, from: data)
-                        completion(prepareStatus, nil, nil)
+                        completion(prepareStatus, sResponse, nil)
                     } catch {
                         //print(error.localizedDescription)
                         //print(error)
                         //print(String(data: data, encoding: .utf8)!)
-                        completion(nil, nil, error)
+                        completion(nil, sResponse, error)
                     }
             }
         }
@@ -46,19 +46,19 @@ open class NET {
                 with: REST.API.Localization.localize.additionalHeaders,
                 with: url,
                 restMethod: .POST) { sData, sResponse, sError in
-                    guard sError == nil else { completion(nil, nil, sError); return }
-                    guard let data = sData else { completion(nil, nil, nil); return }
+                    guard sError == nil else { completion(nil, sResponse, sError); return }
+                    guard let data = sData else { completion(nil, sResponse, nil); return }
                     
                     do {
                         //print(data)
                         print(String(data: data, encoding: .utf8)!)
                         let localizationResponse = try JSONDecoder().decode(LocalizationModel.Localize.Response.self, from: data)
-                        completion(localizationResponse, nil, nil)
+                        completion(localizationResponse, sResponse, nil)
                     } catch {
                         print(error.localizedDescription)
                         print(error)
                         print(String(data: data, encoding: .utf8)!)
-                        completion(nil, nil, error)
+                        completion(nil, sResponse, error)
                     }
             }
             
