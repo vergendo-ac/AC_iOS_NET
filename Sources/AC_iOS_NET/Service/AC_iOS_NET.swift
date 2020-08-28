8850149c684ddae78e6211c59017e71643991b30
Lammax <lammax@mail.ru>
2020-08-28 11:15:14 +0300

    New swagger API try. Localization

--- Sources/AC_iOS_NET/Service/AC_iOS_NET.swift
+++ Sources/AC_iOS_NET/Service/AC_iOS_NET.swift
@@ -95,10 +95,10 @@ open class NET {
         }
         
         public typealias localizationResultSwagger = LocalizationResult
-        public static func localizeSwagger(at serverAddress: String = Servers.addresses[0], for request: LocalizationModel.Localize.Request, completion: @escaping ((_ data: localizationResultSwagger?,_ error: Error?) -> Void)) {
-            guard let url = REST.API.Localization.localize.getUrl(for: serverAddress) else { completion(nil, nil); return }
+        public typealias imageDescription = ImageDescription
+        public static func localizeSwagger(at serverAddress: String = Servers.addresses[0], for image: URL, with description: imageDescription, completion: @escaping ((_ data: localizationResultSwagger?,_ error: Error?) -> Void)) {
             //TODO: add our server address
-            LocalizerAPI.localize(description: ImageDescription(gps: ImageDescriptionGps(latitude: 1212312.12, longitude: 1123123.123)), image: url, apiResponseQueue: .main) { (localizeResult, error) in
+            LocalizerAPI.localize(description: description, image: image, apiResponseQueue: .main) { (localizeResult, error) in
                 completion(localizeResult, error)
             }
         }
